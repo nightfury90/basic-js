5a261bf4f33e448a08e5b7fb1f5993999f47e26c
Dmitriy An <pancake0890@mail.ru>
2020-09-18 12:12:19 +0500

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,64 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(boolean = true) {
+    this.boolean = boolean;
+  }
+
+  encrypt(str, key) {
+    if (!str || !key) throw new Error('Correct parameters are needed!');
+
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = [];
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      let code = str.charCodeAt(i);
+      result.push(code);
+
+      if (code >= 65 && code <= 90) {
+        result[i] += key.charCodeAt((i - count) % key.length) - 65;
+        if (result[i] > 90) result[i] -= 26;
+      }
+      else count++;
+    }
+
+    if (this.boolean) {
+      return String.fromCharCode(...result);
+    } else {
+      return String.fromCharCode(...result.reverse());
+    }
+  }
+
+
+  decrypt(str, key) {
+    if (!(str && key)) throw new Error('Correct parameters are needed!');
+
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = [];
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      let code = str.charCodeAt(i);
+      result.push(code);
+
+      if (code >= 65 && code <= 90) {
+        result[i] -= key.charCodeAt((i - count) % key.length) - 65;
+        if (result[i] < 65) result[i] += 26;
+      }
+      else count++;
+    }
+
+    if (this.boolean) {
+      return String.fromCharCode(...result);
+    }
+    else {
+      return String.fromCharCode(...result.reverse());
+    }
   }
 }
 
